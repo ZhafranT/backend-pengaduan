d4ceb2448043889ad163f624ab4d137c03fa0861
febioirsadad <febioirsadad@yahoo.com>
2022-06-22 14:02:45 +0700

    update statistic 2

--- app/Http/Controllers/statisticController.php
+++ app/Http/Controllers/statisticController.php
@@ -4,23 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Models\Pengaduan;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use DB;
 
 class statisticController extends Controller
 {
     public function index(){
         
-        $pengaduanData = Pengaduan::select(DB::raw("COUNT(*) as count"))
-        ->whereYear("created_at",date('Y'))
-        ->GroupBy(DB::raw("Month(created_at)"))
-        ->pluck('count');
+        // $pengaduanData = Pengaduan::select(DB::raw("COUNT(*) as count"))
+        // ->whereYear("created_at",date('Y'))
+        // ->GroupBy(DB::raw("Month(created_at)"))
+        // ->pluck('count');
 
-        $bulan = Pengaduan::select(DB::raw("MONTHNAME(created_at) as bulan"))
-        ->GroupBy(DB::raw("MONTHNAME(created_at)"))
-        ->pluck('bulan');
+        // $bulan = Pengaduan::select(DB::raw("MONTHNAME(created_at) as bulan"))
+        // ->GroupBy(DB::raw("MONTHNAME(created_at)"))
+        // ->pluck('bulan');
+
+        $data = Pengaduan::select('id','created_at')->get()->groupBy(function($data) {
+            return Carbon::parse($data->created_at)->format('M');
+        });
+
+        foreach($data as $month => $values) {
+            $months[]=$month;
+            $monthCount[]=count($values);
+        }
         
         return view('statistic.statistic', [
-            "title" => "Statistik"
-        ], compact('pengaduanData','bulan'));
+            "title" => "Statistik",
+            'data'=>$data,'months'=>$months,'monthCount'=>$monthCount
+        ]);
     }
 }
--- resources/views/statistic/statistic.blade.php
+++ resources/views/statistic/statistic.blade.php
@@ -5,6 +5,64 @@
 <div class="card-body">
   <h2>Statistik Pengaduan Konsumen</h2>
 
+  {{-- <div class="card">
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+  </div>
+  <script>
+      const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+      ];
+    
+      const data = {
+        labels: labels,
+        datasets: [{
+          label: 'Data Laporan Pengaduan Masuk Tahun 2022',
+          // backgroundColor: 'rgb(75, 192, 192)',
+          // borderColor: 'rgb(75, 192, 192)',
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 1,
+          data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+      };
+    
+      const config = {
+        type: 'bar',
+        data: data,
+        options: {}
+      };
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script>
+      const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
+  </script> --}}
+
   <div class="card">
     <div class="card-header">Data Laporan Pengaduan Masuk Tahun 2022</div>
     <div class="card-body">
@@ -14,8 +72,8 @@
 
   <script src="https://code.highcharts.com/highcharts.js"></script>
   <script>
-    var pengaduanData = <?php echo json_encode($pengaduanData) ?>;
-    var bulan = <?php echo json_encode($bulan) ?>;
+    var pengaduanData = <?php echo json_encode($monthCount) ?>;
+    var bulan = <?php echo json_encode($months) ?>;
     Highcharts.chart('grafik', {
       title : {
         text: 'Grafik Pengaduan Masuk Bulanan'
@@ -27,6 +85,9 @@
         title: {
           text: 'Total Pengaduan'
         }
+      },
+      dataSorting: {
+            enabled: true
       },
       plotOptions : {
         series: {
