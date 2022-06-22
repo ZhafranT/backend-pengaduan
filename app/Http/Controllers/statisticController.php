60c9eeac0ce4cee724c525bbab0184568ad5def2
febioirsadad <febioirsadad@yahoo.com>
2022-06-22 14:20:51 +0700

    statistic fix

--- app/Http/Controllers/statisticController.php
+++ app/Http/Controllers/statisticController.php
@@ -10,17 +10,8 @@ use DB;
 class statisticController extends Controller
 {
     public function index(){
-        
-        // $pengaduanData = Pengaduan::select(DB::raw("COUNT(*) as count"))
-        // ->whereYear("created_at",date('Y'))
-        // ->GroupBy(DB::raw("Month(created_at)"))
-        // ->pluck('count');
 
-        // $bulan = Pengaduan::select(DB::raw("MONTHNAME(created_at) as bulan"))
-        // ->GroupBy(DB::raw("MONTHNAME(created_at)"))
-        // ->pluck('bulan');
-
-        $data = Pengaduan::select('id','created_at')->get()->groupBy(function($data) {
+        $data = Pengaduan::select('id','created_at')->orderBy('created_at', 'asc')->get()->groupBy(function($data) {
             return Carbon::parse($data->created_at)->format('M');
         });
 
@@ -31,7 +22,9 @@ class statisticController extends Controller
         
         return view('statistic.statistic', [
             "title" => "Statistik",
-            'data'=>$data,'months'=>$months,'monthCount'=>$monthCount
+            'data'=>$data,
+            'months'=>$months,
+            'monthCount'=>$monthCount
         ]);
     }
 }
--- resources/views/statistic/statistic.blade.php
+++ resources/views/statistic/statistic.blade.php
@@ -5,64 +5,6 @@
 <div class="card-body">
   <h2>Statistik Pengaduan Konsumen</h2>
 
-  {{-- <div class="card">
-    <div>
-        <canvas id="myChart"></canvas>
-    </div>
-  </div>
-  <script>
-      const labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-      ];
-    
-      const data = {
-        labels: labels,
-        datasets: [{
-          label: 'Data Laporan Pengaduan Masuk Tahun 2022',
-          // backgroundColor: 'rgb(75, 192, 192)',
-          // borderColor: 'rgb(75, 192, 192)',
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'
-          ],
-          borderWidth: 1,
-          data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-      };
-    
-      const config = {
-        type: 'bar',
-        data: data,
-        options: {}
-      };
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  <script>
-      const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-      );
-  </script> --}}
-
   <div class="card">
     <div class="card-header">Data Laporan Pengaduan Masuk Tahun 2022</div>
     <div class="card-body">
@@ -76,14 +18,14 @@
     var bulan = <?php echo json_encode($months) ?>;
     Highcharts.chart('grafik', {
       title : {
-        text: 'Grafik Pengaduan Masuk Bulanan'
+        text: "Grafik Pengaduan Masuk Bulanan"
       },
       xAxis : {
         categories: bulan
       },
       yAxis : {
         title: {
-          text: 'Total Pengaduan'
+          text: "Total Pengaduan"
         }
       },
       dataSorting: {
@@ -96,7 +38,7 @@
       },
       series : [
         {
-          name: 'Jumlah Pengaduan',
+          name: "Jumlah Pengaduan",
           data: pengaduanData
         }
       ]
