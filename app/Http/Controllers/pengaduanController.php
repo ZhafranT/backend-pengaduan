5a9fe523443f7246779840f89caed0ea5326f8b6
febioirsadad <febioirsadad@yahoo.com>
2022-07-01 14:16:20 +0700

    update bukti base 64 2

--- app/Http/Controllers/pengaduanController.php
+++ app/Http/Controllers/pengaduanController.php
@@ -272,7 +272,7 @@ class pengaduanController extends Controller
             
             $type = $request->file('buktiPembelian')->extension();
             $imagedata = file_get_contents($request->file('buktiPembelian'));
-            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($imagedata);
+            // $base64 = 'data:image/' . $type . ';base64,' . base64_encode($imagedata);
 
             $transaction = Pengaduan::create([
                 'user_id' => $validatedData['user_id'],
@@ -298,7 +298,7 @@ class pengaduanController extends Controller
                 'jenisPengaduan' => $validatedData['jenisPengaduan'],
                 'waktuKejadianDitemukan' => $validatedData['waktuKejadianDitemukan'],
                 'tempatLokasiKejadian' => $validatedData['tempatLokasiKejadian'],
-                'buktiPembelian' => $base64,
+                'buktiPembelian' => $imagedata,
                 'saksi' => $validatedData['saksi'],
                 'kerugianMaterial' => $validatedData['kerugianMaterial'],
                 'kerugianFisik' => $validatedData['kerugianFisik'],
